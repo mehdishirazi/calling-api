abf40c7c0acf1b124cddc13003eba6d1f34a86d8
mehdishirazi <49481094+mehdishirazi@users.noreply.github.com>
2020-03-25 02:29:46 +0430

    Implemented the CreateElement and the AddHeader methods (#1)
    * Enhancement on rewising codes
    
    * Fixing bug on CreateElement function
    
    * Fixing bug on CreateElement function
    
    * Enhancement on variable's names
    
    * Enhancment on variables and function's working
    
    * Deleting two extra names
    
    * Changing one of names of CreateElement function parameters
    
    * Enhancment on AddHeader function
    
    * Fixing names and errors
    
    * Fixing wrong names
    
    * Adding countOfHeader to AddHeader function
    
    Co-authored-by: mehdi shirazi <msh199271@gmail.com>

--- calling-api.js
+++ calling-api.js
@@ -2,87 +2,33 @@ let countOfHeader = 0;
 let countOfBody = 0;
 
 
-function CreateElement(elementName, append, id=null){
+function CreateElement(elementName, append=null, id=null, innerText=null){
     element = document.createElement(elementName);
+    if (append !== null){
+        append.appendChild(element);
+    }
     if (id !== null){
         element.id = id;
     }
-    append.appendChild(element);
+    if (innerText !== null){
+        element.innerText = innerText;
+    }
     return element;
 }
 
+
 function AddHeader(){
-  debugger
-    let text_key = document.createTextNode('Key');
-    let text_value = document.createTextNode('Value');
-    let header = document.getElementById('headerKey');
-    let head = document.getElementById('header');
-    let value = document.getElementById('headerValue');
-    let value_br = document.createElement('br');
-    let input_br = document.createElement('br');
+    let headerKey = document.getElementById('headerKey');
+    let headerValue = document.getElementById('headerValue');
     
-    CreateElement("span", header);
-    element.appendChild(text_key);
-    CreateElement("input", header, id="inputKey"+countOfHeader);
+    CreateElement("span", headerKey, id=null, innerText = "Key");
+    CreateElement("input", headerKey, id="inputKey"+countOfHeader);
 
-    CreateElement("span", value);
-    element.appendChild(text_value);
-    element.after(value_br);
-    CreateElement("input", value, id="inputValue"+countOfHeader);
-    element.after(input_br);
+    CreateElement("span", headerValue, id=null, innerText = "Value");
+    CreateElement("br", headerValue);
+    CreateElement("input", headerValue, id="inputValue"+countOfHeader);
+    CreateElement("br", headerValue);
+
+    countOfHeader++;
 }
 
-
-function NewBody(){
-    let tags = [];
-
-    for (i=1; i < 3; i++){
-        input_i = document.createElement('INPUT');    
-        span_i= document.createElement('SPAN');
-        br_i = document.createElement('br');
-        input_i.id = "input_body" + i;
-        tags.push(span_i);
-        tags.push(input_i);
-        tags.push(br_i);
-    }
-
-    let mybr = document.createElement('br');
-    let body = document.getElementById('bodyDiv');
-    let head = document.getElementById('body');
-    let key_value = document.getElementById('btnDiv');
-    let btn = document.getElementById('body_btn');
-    let text_key = document.createTextNode('Key');
-    let text_value = document.createTextNode('Value');
-  
-    body.appendChild(tags[0]); 
-    body.appendChild(tags[1]);
-    body.appendChild(tags[2]);
-    tags[0].appendChild(text_key);
-    head.after(tags[0]);
-    tags[0].after(tags[1]);
-    tags[0].before(tags[2]);
-    
-    key_value.appendChild(tags[5]); 
-    key_value.appendChild(mybr); 
-    key_value.appendChild(tags[3]); 
-    key_value.appendChild(tags[4]);
-    tags[3].appendChild(text_value);
-    btn.after(tags[3]);
-    tags[3].after(tags[4]);
-    tags[3].before(tags[5]);
-    tags[4].before(mybr); 
-}
-
-
-function Send(){
-  debugger
-    let method = document.getElementById('verbs').value;
-    let url = document.getElementById('item1').value;
-    let header = {};
-    let body = {};
-    let spin = document.getElementById("value").getElementsByTagName("input");
-    
-    for (i = 1; i <= spin.length; i++){
-    let x = document.getElementById('input_header'+ i).value;
-    }
-}
