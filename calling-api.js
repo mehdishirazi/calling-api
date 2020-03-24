853dc9eb192540e84e62017ed251d6d6f360d3ac
mehdi shirazi <msh199271@gmail.com>
2020-03-25 01:56:43 +0430

    Enhancment on variables and function's working

--- calling-api.js
+++ calling-api.js
@@ -2,13 +2,16 @@ let countOfHeader = 0;
 let countOfBody = 0;
 
 
-function CreateElement(elementName, append=null, id=null){
+function CreateElement(elementName, append=null, id=null, textName=null){
     element = document.createElement(elementName);
     if (append !== null){
         append.appendChild(element);
     }
     if (id !== null){
         element.id = id;
+    }
+    if (elementName == "span" && textName !== null){
+        element.innerText = textName;
     }
     return element;
 }
@@ -20,16 +23,12 @@ function AddHeader(){
     let headerKey = document.getElementById('headerKey');
     let headerValue = document.getElementById('headerValue');
     
-    createElement = CreateElement;
-    
-    createElement("span", headerKey);
-    element.appendChild(textKey);
-    createElement("input", headerKey, id="inputKey"+countOfHeader);
+    CreateElement("span", headerKey, id=null, textName = "Key");
+    CreateElement("input", headerKey, id="inputKey"+countOfHeader);
 
-    createElement("span", headerValue);
-    element.appendChild(textValue);
-    element.after(document.createElement('br'));
-    createElement("input", headerValue, id="inputValue"+countOfHeader);
-    element.after(document.createElement('br'));
+    let spanTag = CreateElement("span", headerValue, id=null, textName = "Value");
+    spanTag.after(document.createElement('br'));
+    let inputTag = CreateElement("input", headerValue, id="inputValue"+countOfHeader);
+    inputTag.after(document.createElement('br'));
 }
 
