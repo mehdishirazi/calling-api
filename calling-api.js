ae2133e283e2c81d20558488354a5014d361f94a
mehdishirazi <49481094+mehdishirazi@users.noreply.github.com>
2020-04-10 15:09:27 +0430

    Implementing deleting headers and body's items in dom (#12)
    * Added new function
    
    * Added selected row function for deleting items in dom
    
    * Changing the name of SelectedRow function to DeletingRow function
    
    * Fixing bug on not showing tables headers when calling an Api (#13)
    
    Co-authored-by: Mehdi Shirazi <msh199271@gmail.com>
    
    * Added new method of deleting rows
    
    * Deleting extra tag id
    
    Co-authored-by: Mehdi Shirazi <msh199271@gmail.com>

--- calling-api.js
+++ calling-api.js
@@ -2,7 +2,7 @@ let countOfHeader = 0;
 let countOfBody = 0;
 
 
-function CreateElement(elementName, append=null, id=null, innerText=null){
+function CreateElement(elementName, append=null, id=null, innerText=null, className=null){
     element = document.createElement(elementName);
     if (append !== null){
         append.appendChild(element);
@@ -13,6 +13,9 @@ function CreateElement(elementName, append=null, id=null, innerText=null){
     if (innerText !== null){
         element.innerText = innerText;
     }
+    if (className !== null){
+        element.className = className;
+    }
     return element;
 }
 
@@ -21,13 +24,21 @@ function AddHeader(){
     let headerKey = document.getElementById('headerKey');
     let headerValue = document.getElementById('headerValue');
     
-    CreateElement("span", headerKey, id=null, innerText = "Key");
+    CreateElement("span", headerKey, id="spanHeaderKey"+countOfHeader, innerText = "Key");
     CreateElement("input", headerKey, id="inputHeaderKey"+countOfHeader);
 
-    CreateElement("span", headerValue, id=null, innerText = "Value");
-    CreateElement("br", headerValue);
-    CreateElement("input", headerValue, id="inputHeaderValue"+countOfHeader);
-    CreateElement("br", headerValue);
+    CreateElement("span", headerValue, id="spanHeaderValue"+countOfHeader, innerText = "Value");
+    CreateElement("br", headerValue, id="br"+countOfHeader);
+    CreateElement("input", headerValue, id="inputHeaderValue"+countOfHeader, innerText=null);
+    let btn = CreateElement(
+        elementName="button", 
+        append=headerValue, 
+        id="btnHeader"+countOfHeader,
+        innerText="Delete",
+        className="deleteBtn"
+    )
+    btn.onclick = DeletingRow;
+    CreateElement("br", headerValue, id="secondBr"+countOfHeader);
 
     countOfHeader++;
 }
@@ -37,13 +48,21 @@ function AddBody(){
     let bodyDiv = document.getElementById('bodyDiv');
     let bodyValue = document.getElementById('bodyValue');
     
-    CreateElement("span", bodyDiv, id=null, innerText = "Key");
+    CreateElement("span", bodyDiv, id="spanBodyKey"+countOfBody, innerText = "Key");
     CreateElement("input", bodyDiv, id="inputBodyKey"+countOfBody);
 
-    CreateElement("span", bodyValue, id=null, innerText = "Value");
-    CreateElement("br", bodyValue);
+    CreateElement("span", bodyValue, id="spanBodyValue"+countOfBody, innerText = "Value");
+    CreateElement("br", bodyValue, id="br"+countOfBody);
     CreateElement("input", bodyValue, id="inputBodyValue"+countOfBody);
-    CreateElement("br", bodyValue);
+    let btn = CreateElement(
+        elementName="button", 
+        append=bodyValue, 
+        id="btnBody"+countOfBody,
+        innerText="Delete",
+        className="deleteBtn"
+    )
+    btn.onclick = DeletingRow;
+    CreateElement("br", bodyValue, id="secondBr"+countOfBody);
 
     countOfBody++;
 }
@@ -123,6 +142,37 @@ function ShowingResults(fetchApi){
             let cell_z = row.insertCell(z);
             cell_z.innerHTML = fetchApi[j][keys[z]];
         }
+    }
+}
+
+
+function DeletingRow(e){
+    let eventId = e.currentTarget.id;
+    let selected = eventId.match(/Header|Body/g).toString();
+    let selectedNumber = eventId.match(/\d/g).toString();
+    
+    if (selected == "Header"){
+        let parent = Array.from(document.querySelectorAll('[id^="header"]'));
+        let childKey = Array.from(parent[0].querySelectorAll(`[id$="${selectedNumber}"]`));
+        let childValue = Array.from(parent[1].querySelectorAll(`[id$="${selectedNumber}"]`));
+        for(i=0; i < childKey.length; i++){
+            parent[0].removeChild(childKey[i]);
+        } 
+        for(i=0; i < childValue.length; i++){
+            parent[1].removeChild(childValue[i]);
+        } 
+    }
+    
+    if (selected == "Body"){
+        let parent = Array.from(document.querySelectorAll('[id^="body"]'));
+        let childKey = Array.from(parent[0].querySelectorAll(`[id$="${selectedNumber}"]`));
+        let childValue = Array.from(parent[1].querySelectorAll(`[id$="${selectedNumber}"]`));
+        for(i=0; i < childKey.length; i++){
+            parent[0].removeChild(childKey[i]);
+        } 
+        for(i=0; i < childValue.length; i++){
+            parent[1].removeChild(childValue[i]);
+        } 
     }
 }
 
